c3337a59215b2ac9dc38be0a9d03a0755f968f5f
AnuragNegii <negianurag99@gmail.com>
2023-03-03 20:48:31 +0530

    Added Music and Option Panel

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,22 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
     [SerializeField] private AudioSource gameMusic, gameSFX;
    
     [Header("All The Music That Can be used in Music")]
-    [SerializeField] private AudioClip[] GameMusic;
+    [SerializeField] private AudioClip[] gameMusicClips;
 
     [Header("All The Sound effects That Can be used in Music")]
-    [SerializeField] private AudioClip[] GameSFX;
+    [SerializeField] private AudioClip[] gameSFXClips;
 
-    AudioSource audioSource;
+    [SerializeField] private Slider musicVolumeSlider, sfxVolumeSlider;
+
 
     private void Awake() {
-        audioSource = FindObjectOfType<AudioSource>();
+        
+    }
+
+    private void Update() {
+        MusicVolumeSlider();
+        SFXVolumeSlider();
+    }
+    private void MusicVolumeSlider(){
+        musicVolumeSlider.value = gameMusic.volume;
+    }
+
+    private void SFXVolumeSlider(){
+        sfxVolumeSlider.value = gameSFX.volume;
     }
 
 
+    public void ToggleSFX(){
+        gameSFX.mute = !gameSFX.mute;
+    }
+
+    public void ToggleMusic(){
+        gameMusic.mute = !gameMusic.mute;
+    }
 }
