b15b2d56e1977c6692de198ed0dd39112fc920eb
AnuragNegii <negianurag99@gmail.com>
2023-03-04 11:22:05 +0530

    Added music to the game

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -2,22 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
 public class AudioManager : MonoBehaviour
-{
+{   
+    public static AudioManager Instance;
     [SerializeField] private AudioSource gameMusic, gameSFX;
    
     [Header("All The Music That Can be used in Music")]
     [SerializeField] private AudioClip[] gameMusicClips;
 
-    [Header("All The Sound effects That Can be used in Music")]
+    [Header("All The Sound That Can be used in SFX")]
     [SerializeField] private AudioClip[] gameSFXClips;
 
     [SerializeField] private Slider musicVolumeSlider, sfxVolumeSlider;
 
 
     private void Awake() {
-        
+        if(Instance == null){
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start() {
+        PlayMusic();
+    }
+
+    private void PlayMusic(){
+        if(gameMusicClips[0] == null){
+            return;
+        }else{  
+            gameMusic.PlayOneShot(gameMusicClips[0]);
+        }
+
     }
 
     private void Update() {
---
+++ Assets/Scripts/MusicAndAudio.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class MusicAndAudio
+{
+    [SerializeField]private string name;
+    [SerializeField] private AudioClip clip;
+
+}
