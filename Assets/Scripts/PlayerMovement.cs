23ed6285e37fe50bcff951f62022bb9c86d2e61e
AnuragNegii <negianurag99@gmail.com>
2023-03-16 17:20:32 +0530

    gameplay

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5f;
+    private void Update() {
+        if(Input.GetKeyDown(KeyCode.A)){
+
+        }
+    }
+}
